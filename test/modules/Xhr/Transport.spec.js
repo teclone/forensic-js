956b5e885c3172c5c55c2c522a9c0ff66a729b0a
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-29 11:04:15 +0100

    test: create test specs for the xhr transport module

---
+++ test/modules/Xhr/Transport.spec.js
@@ -0,0 +1,22 @@
+import Transport from '../../../src/modules/Xhr/Transport.js';
+
+describe('Transport', function() {
+    describe('.create()', function() {
+        it(`should create an xhr transport object`, function() {
+            expect(Transport.create()).to.be.an('XMLHttpRequest');
+        });
+    });
+
+    describe('.supported', function() {
+        it(`should return a boolean indicating if transport objects can be created`, function() {
+            expect(Transport.supported).to.be.true;
+        });
+    });
+
+    describe('.ieString', function() {
+        it(`should return a string indicating the MSXML version string used in created the
+        xmlhttprequest activeXobject, assuming it is running in ie`, function() {
+            expect(Transport.ieString).to.be.string;
+        });
+    });
+});
