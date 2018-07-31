e4c7d0c8245b3f2c2401f885a07a98d773b86f22
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-31 21:35:21 +0100

    refactor: add more route endpoints for our server end to end test

--- server/app.js
+++ server/app.js
@@ -6,4 +6,25 @@ app.get('/', (req, res) => {
     res.end('Hello World');
 });
 
+//prolong request
+app.get('prolong-response', () => {
+    //do nothing
+});
+
+//report request header
+app.all('report-header/{header}', (request, response, header) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.end(request.headers[header]);
+});
+
+//report query
+app.all('report-query', (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/json'
+    });
+    response.end(JSON.stringify(request.query));
+});
+
 module.exports = app;
