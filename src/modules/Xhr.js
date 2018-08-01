dbd19406d3099778eb85d8fdf274477cc026dc70
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-01 19:14:13 +0100

    feat: add polling, request prioritization and request promotion capability to the Xhr fetch manager.
    The manager has an inbuilt mechanism to poll for request, prioritize request and promote requests. I
    am grateful to Nicholas C.Zakas for his previous work on this in his book title professional Ajax,
    chapter 5

--- src/modules/Xhr.js
+++ src/modules/Xhr.js
@@ -4,6 +4,7 @@
  *@module Xhr
 */
 import Queue from './Queue.js';
+import _Response from './Xhr/Response.js';
 
 /**
  *@name xhrStates
@@ -11,43 +12,124 @@ import Queue from './Queue.js';
 */
 let xhrStates = {
 
-    /**
-     * default request timeout value.
-    */
-    timeoutAfter: 15000,
+        /**
+         * default request timeout value.
+        */
+        timeoutAfter: 15000,
+
+        /**
+         * boolean value to indicate if manager has been started
+        */
+        started: false,
+
+        /**
+         * time in milliseconds to poll requests in a repeating circle
+        */
+        pollAfter: 250,
+
+        /**
+         * this is the number of milliseconds a request will stay and its priority level will be
+         * promoted
+        */
+        promoteAfter: 3000,
+
+        /**
+         * pending requests
+        */
+        pendingRequests: new Queue(null, true, false, (one, two) => {
+            return Queue.fnSort(one.priority, two.priority);
+        }),
+
+        /**
+         * active requests
+        */
+        activeRequests: new Queue(null, false),
+
+        /**
+         * global headers
+        */
+        globalHeaders: {
+
+        }
+    },
 
     /**
-     * boolean value to indicate if manager has been started
+     *@private
+     * checks for request that have completed, initaiting the appropriate callback
     */
-    started: false,
+    checkActiveRequests = function () {
+        xhrStates.activeRequests.forEach(function(request, idx, queue) {
+            if (request.state === 'complete') {
+                queue.deleteIndex(idx);
+
+                let response = new _Response(request);
+                if (response.ok)
+                    request.resolve(response);
+                else
+                    request.reject(response);
+            }
+        });
+    },
 
     /**
-     * time in milliseconds to poll requests in a repeating circle
+     *@private
+     * executes next request
     */
-    pollAfter: 250,
+    executeNextRequest = function () {
+        let activeRequests = xhrStates.activeRequests,
+            pendingRequests = xhrStates.pendingRequests;
+
+        while(activeRequests.length < 4 && pendingRequests.length > 0) {
+            let request = pendingRequests.shift();
+
+            request.send();
+
+            request.stayed = 0;
+            activeRequests.put(request);
+        }
+    },
 
     /**
-     * this is the number of milliseconds a request will stay and its priority level will be
-     * promoted
+     *@private
+     * promotes the priority of each pending request that have lasted for ageLimit
     */
-    promoteAfter: 3000,
+    promote = function () {
+        var pendingRequests = xhrStates.pendingRequests;
+        pendingRequests.forEach((request) => {
+            request.age += xhrStates.pollAfter;
+            if (request.age >= xhrStates.promoteAfter) {
+                request.age = 0;
+                request.priority -= 1;
+            }
+        });
+        pendingRequests.sort();
+    },
 
     /**
-     * pending requests
+     *@private
+     * manages the polling process.
     */
-    pendingRequests: new Queue(null, true, false, (one, two) => {
-        return Queue.fnSort(one.priority, two.priority);
-    }),
+    manage = function() {
+        clearTimeout(xhrStates.iterationId);
+
+        promote();
+        executeNextRequest();
+        checkActiveRequests();
+
+        if (xhrStates.pendingRequests.length > 0 || xhrStates.activeRequests.length > 0)
+            xhrStates.iterationId = setTimeout(manage, xhrStates.pollAfter);
+        else
+            xhrStates.started = false;
+    },
 
     /**
-     * active requests
+     *@private
+     * starts the polling processing.
     */
-    activeRequests: new Queue(null, false),
+    start = function() {
+        if (xhrStates.started)
+            return;
 
-    /**
-     * global headers
-    */
-    globalHeaders: {
-
-    }
-};
+        xhrStates.iterationId = setTimeout(manage, xhrStates.pollAfter);
+        xhrStates.started = true;
+    };
