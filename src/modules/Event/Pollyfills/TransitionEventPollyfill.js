4aa82fa06f1bf9b90088018204e68b43da52730f
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-10 21:54:56 +0100

    feat: create TransitionEvent pollyfill for environments that do not support it, including jsdom

---
+++ src/modules/Event/Pollyfills/TransitionEventPollyfill.js
@@ -0,0 +1,78 @@
+/**
+ * event initialization options.
+ *@typedef {Object} TransitionEventInit
+ *@property {boolean} [TransitionEventInit.bubbles=true] - boolean value indicating if event bubbles
+ *@property {boolean} [TransitionEventInit.cancelable=false] - boolean value indicating if event is cancelable
+ *@property {string} [TransitionEventInit.propertyName=''] - transition property name
+ *@property {float} [TransitionEventInit.elapsedTime=0.0] - transition elapsed time
+ *@property {string} [TransitionEventInit.pseudoElement=''] - transition pseudo element
+*/
+import { createDOMEvent } from '../../Globals.js';
+import Util from '../../Util.js';
+
+let installed = false;
+
+export default {
+    install(host, initEvent) {
+        /* istanbul ignore if */
+        if (installed)
+            return;
+
+        /* istanbul ignore if */
+        if (createDOMEvent('TransitionEvent', 'transitionstart') !== null) {
+            installed = true;
+            return;
+        }
+
+        /**
+         *@param {string} type - the event type
+         *@param {TransitionEventInit} eventInit - the event init object
+        */
+        host.TransitionEvent = function(type, eventInit) {
+            /* istanbul ignore if */
+            if (!Util.isPlainObject(eventInit))
+                eventInit = {};
+
+            let event = createDOMEvent('Event', type, initEvent({}, eventInit), ['bubbles', 'cancelable']);
+
+            Object.defineProperties(event, {
+                [Symbol.toStringTag]: {
+                    get() {
+                        return 'TransitionEvent';
+                    }
+                },
+
+                /**
+                 *@type {string}
+                */
+                propertyName: {
+                    get() {
+                        return eventInit.propertyName;
+                    }
+                },
+
+                /**
+                 *@type {string}
+                */
+                pseudoElement: {
+                    get() {
+                        return eventInit.pseudoElement;
+                    }
+                },
+
+                /**
+                 *@type {number}
+                */
+                elapsedTime: {
+                    get() {
+                        return eventInit.elapsedTime;
+                    }
+                }
+            });
+
+            return event;
+        };
+
+        installed = true;
+    }
+};
