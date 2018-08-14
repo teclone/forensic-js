404fb56342c792144c008a4499695387b47eae9b
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-14 13:21:03 +0100

    feat: add unified selectNode method, namespace resolver and argument validator
    The selectNode method unifies IE selectSingleNode and Dom implementation FIRST_ORDERED_NODE_TYPE
    evaluate call

--- src/modules/XPath.js
+++ src/modules/XPath.js
@@ -1,4 +1,5 @@
-import { onInstall, install, uninstall } from './Globals.js';
+import { onInstall, host, install, uninstall } from './Globals.js';
+import Util from './Util.js';
 import XML from './XML.js';
 
 let xPathStates = {
@@ -11,6 +12,61 @@ let xPathStates = {
          * tells the kind of xPath implementation used
         */
         implementation: 0
+    },
+
+    /**
+     * resolves namespace specific to ActiveXObject implementation only and returns the
+     * resolved namespace or empty string
+     *
+     *@param {Object} [namespaces] - the namespace object
+     *@returns {string}
+    */
+    /* istanbul ignore next */
+    resolveActiveXObjectNamespace = function (namespaces) {
+        if (!Util.isPlainObject(namespaces))
+            return '';
+        else
+            return Object.keys(namespaces).map(key => {
+                return `xmlns:${key}="${namespaces[key]}"`;
+            }).join(' ');
+    },
+
+    /**
+     * returns a dom implementation namesapace resolver for the given namespace.
+     *@param {Object} [namespaces] - the namespace object
+     *@returns {Function|null}
+    */
+    resolveDOMImplementationNamespace = function (namespaces) {
+        if (Util.isPlainObject(namespaces))
+            return function(prefix) {
+                /* istanbul ignore else */
+                if (typeof namespaces[prefix] !== 'undefined')
+                    return namespaces[prefix];
+                else
+                    return null;
+            };
+        else
+            return null;
+    },
+
+    /**
+     * validates the selector and node
+     *@param {string} selector - the xPath selector
+     *@param {Document|Element} node - the context node
+     *@returns {Document}
+    */
+    validate = function(selector, node) {
+        if (typeof selector !== 'string')
+            throw new TypeError(selector + ' is not an xPath selector');
+
+        if (!Util.isElementNode(node) && !Util.isDocumentNode(node))
+            throw new TypeError(node + ' is not a valid xPath selection context node');
+
+        /* istanbul ignore if */
+        if (!xPathStates.supported)
+            throw new Error('XPath is not supported');
+
+        return Util.isElementNode(node)? node.ownerDocument : node;
     },
 
     /**
@@ -73,5 +129,28 @@ export default {
     */
     get implementation() {
         return xPathStates.implementation;
+    },
+
+    /**
+     * selects the first matching node or null if there is no match
+     *@param {string} selector - the xPath selector
+     *@param {Document|Element} node - the context node
+     *@param {Object} [namespaces] - the namespaces object
+     *@returns {Node|null}
+    */
+    selectNode(selector, node, namespaces) {
+        let reference = validate(selector, node);
+        /* istanbul ignore if */
+        if (xPathStates.implementation == 1) {
+            reference.setProperty('SelectionNamespaces', resolveActiveXObjectNamespace(namespaces));
+            return node.selectSingleNode(selector);
+        }
+        else {
+            let xPathResult = reference.evaluate(
+                selector, node, resolveDOMImplementationNamespace(namespaces),
+                host.XPathResult.FIRST_ORDERED_NODE_TYPE, null
+            );
+            return xPathResult.singleNodeValue;
+        }
     },
 };
