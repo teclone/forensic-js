1c6a745912ac51d08946a9b7f760a3806b1950a8
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-14 13:18:29 +0100

    feat: create XPath module export, detect platform implementation type

--- src/modules/XPath.js
+++ src/modules/XPath.js
@@ -1,4 +1,4 @@
-import { onInstall} from './Globals.js';
+import { onInstall, install, uninstall } from './Globals.js';
 import XML from './XML.js';
 
 let xPathStates = {
@@ -34,3 +34,44 @@ let xPathStates = {
     };
 
 onInstall(init);
+
+export default {
+    /**
+     * calls the Globals install method with the parameters. This is useful when using the
+     * Utils module as a standalone distribution or lib.
+     *
+     *@param {Object} hostParam - the host object, the global this object in a given usage
+     * environment
+     *@param {Object} rootParam - the root object. an example is the document object
+     *@returns {boolean}
+    */
+    install(hostParam, rootParam) {
+        return install(hostParam, rootParam);
+    },
+
+    /**
+     * calls the Globals uninstall method with the parameters. This is useful when using the
+     * Utils module as a standalone distribution or lib.
+     *
+     *@returns {boolean}
+    */
+    uninstall() {
+        return uninstall();
+    },
+
+    /**
+     * indicates if xPath is supported
+     *@type {boolean}
+    */
+    get supported() {
+        return xPathStates.supported;
+    },
+
+    /**
+     * indicates the XPath implementation type that is supported
+     *@type {number}
+    */
+    get implementation() {
+        return xPathStates.implementation;
+    },
+};
