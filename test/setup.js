ad478efc3b21130279c0837156b6be6d15208b85
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-15 20:54:17 +0100

    test: only set the host and root global objects once, to avoid conflicting results when running test

--- test/setup.js
+++ test/setup.js
@@ -1,39 +1,41 @@
 let JSDOM = require('jsdom').JSDOM;
 
-/**
- * create dom to be used for testing
-*/
-let dom = new JSDOM('<!doctype html><html><body></body></html>', {
-    url: 'http://127.0.0.1:4000',
-    pretendToBeVisual: true
-});
+if (typeof global.window === 'undefined') {
+    /**
+     * create dom to be used for testing
+    */
+    let dom = new JSDOM('<!doctype html><html><body></body></html>', {
+        url: 'http://127.0.0.1:4000',
+        pretendToBeVisual: true
+    });
 
-/**
- * set up test global variables including a test server to be used for http requests
-*/
-global.expect = require('chai').expect;
-global.server = require('../server/app.js');
-global.window = dom.window;
-global.document = window.document;
+    /**
+     * set up test global variables including a test server to be used for http requests
+    */
+    global.expect = require('chai').expect;
+    global.server = require('../server/app.js');
+    global.window = dom.window;
+    global.document = window.document;
 
-window.XMLSerializer = require('@harrison-ifeanyichukwu/xml-serializer');
+    window.XMLSerializer = require('@harrison-ifeanyichukwu/xml-serializer');
 
-require('wicked-good-xpath').install(window, true);
+    require('wicked-good-xpath').install(window, true);
 
-/**
- * a workaround to istanbul's branching error on constructors that extends other constructors.
-*/
-global.getInstance = function(className, ...parameters) {
-    let proto = className.__proto__;
+    /**
+     * a workaround to istanbul's branching error on constructors that extends other constructors.
+    */
+    global.getInstance = function(className, ...parameters) {
+        let proto = className.__proto__;
 
-    className.__proto__ = null;
-    try {
-        new className(...parameters);
-    }
-    catch(ex) {
-        //
-    }
+        className.__proto__ = null;
+        try {
+            new className(...parameters);
+        }
+        catch(ex) {
+            //
+        }
 
-    Object.setPrototypeOf(className, proto);
-    return new className(...parameters);
-};
+        Object.setPrototypeOf(className, proto);
+        return new className(...parameters);
+    };
+}
