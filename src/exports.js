a0421ed60aaa9dcd73cbdf427a75d83b3a3a3ddf
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-08-17 00:41:01 +0100

    feat: export all user facing library modules

---
+++ src/exports.js
@@ -0,0 +1,7 @@
+export {install, uninstall} from './modules/Globals.js';
+export {default as Util} from './modules/Util.js';
+export {default as Queue} from './modules/Queue.js';
+export {default as Event} from './modules/Event.js';
+export {default as Xhr} from './modules/Xhr.js';
+export {default as XML} from './modules/XML.js';
+export {default as XPath} from './modules/XPath.js';
